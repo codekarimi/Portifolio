021b93660aca48a17535ce79d0be924f24820709
codekarimi <ckarimi676@gmail.com>
2025-02-22 12:00:41 +0300

    navigation modification

--- data/index.ts
+++ data/index.ts
@@ -1,5 +1,6 @@
 export const navItems = [
-    { name: "About", link: "#about" },
+    { name: "Home", link: "#home" },
+    // { name: "About", link: "#about" },
     { name: "Projects", link: "#projects" },
     { name: "Testimonials", link: "#testimonials" },
     { name: "Contact", link: "#contact" },
