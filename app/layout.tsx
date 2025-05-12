18ab8480110eb1c254315d1a0f2766a23ebbcb7a
codekarimi <ckarimi676@gmail.com>
2025-05-12 20:51:20 +0300

    MetaData

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,7 +7,7 @@ import { ThemeProvider } from "@/components/theme-provider";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "Clifford's Portfolio",
+  title: "Codekarimi's Portfolio",
   description: "Modern & Minimal JS Mastery Portfolio",
 };
 
